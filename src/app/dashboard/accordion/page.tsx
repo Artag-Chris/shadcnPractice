7941990cccfc7390c95180234d741ac8381ebca4
Artag/Chris <scristxyz@gmail.com>
2024-10-28 09:16:48 -0500

    first commit

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,29 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function Page() {
   return (
     <div>
-      <h1>Accordion Page</h1>
+      <Accordion type="multiple">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+          <AccordionContent>otro texto</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>otro valore?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+          <AccordionContent>otro texto</AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
