6b7551015c06368ad747bed5e32fe7b3135ab20f
Artag/Chris <scristxyz@gmail.com>
2024-10-28 10:06:08 -0500

    act

---
+++ src/app/dashboard/badge/page.tsx
@@ -0,0 +1,9 @@
+import { Badge } from "@/components/ui/badge";
+
+export default function Page() {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      <Badge variant="default">Default</Badge>
+    </div>
+  );
+}
